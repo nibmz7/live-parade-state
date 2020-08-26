289394bfa318a67991544f1cb1fcf313e81579c0
nibmz7 <nibmz7@gmail.com>
2020-08-26 13:59:37 +0800

    Orgainze file structure to TS migration

---
+++ es-dev-server.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  port: 8080,
+  watch: true,
+  nodeResolve: true,
+  open: true,
+  rootDir: "./",
+  basePath: './',
+  appIndex: 'src/index.html',
+  preserveSymlinks: true,
+  plugins: [],
+  moduleDirs: ["node_modules"]
+};
