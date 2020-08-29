762ae4451460ece85606acd00ddaada635f26a32
nibmz7 <nibmz7@gmail.com>
2020-08-30 00:13:56 +0800

    Added department reducer

---
+++ data/reducers/department_reducer.ts
@@ -0,0 +1,36 @@
+import { ACTION_ROOT, Action } from '../../data/store';
+import {
+  DepartmentStoreState,
+  DepartmentAction,
+  ACTION_TYPE
+} from 'data/states/department_state';
+
+const initialState: DepartmentStoreState = {
+  state: 0,
+  items: {}
+};
+
+export const department = (
+  state: DepartmentStoreState = initialState,
+  rootAction: Action
+): DepartmentStoreState => {
+  if (rootAction.root === ACTION_ROOT.RESET) return initialState;
+  if (rootAction.root !== ACTION_ROOT.DEPARTMENTS) return state;
+
+  const action = rootAction as DepartmentAction;
+  const department = action.department;
+  const items = state.items;
+  if (
+    action.type === ACTION_TYPE.ADDED ||
+    action.type === ACTION_TYPE.MODIFIED
+  ) {
+    items[department.id] = department;
+  } else if (action.type === ACTION_TYPE.REMOVED) {
+    delete items[department.id];
+  }
+
+  return {
+    items,
+    ...action
+  };
+};
