72debd058de12184467b1ae5dfd7f4f1397f893e
nibmz7 <nibmz7@gmail.com>
2020-08-30 21:15:56 +0800

    Added department reducer test

---
+++ test/data/reducers/department_reducer.test.ts
@@ -0,0 +1,45 @@
+import ACTION_DEPARTMENT from '../../../data/actions/department_action';
+import { MockModel } from '../../../data-mock/mock_data';
+import { department } from '../../../data/reducers/department_reducer';
+import { DepartmentStoreState } from '../../../data/states/department_state';
+import { expect } from 'chai';
+
+describe('Department reducer', () => {
+  it('Request add', () => {
+    let action = ACTION_DEPARTMENT.requestAdd(MockModel.Department);
+    let reduce = department(undefined, action);
+    let expectedResult: DepartmentStoreState = {
+      action,
+      items: {}
+    };
+    expect(reduce).deep.equal(expectedResult);
+  });
+
+  it('Add', () => {
+    let action = ACTION_DEPARTMENT.added(MockModel.Department);
+    let reduce = department(undefined, action);
+    let expectedResult: DepartmentStoreState = {
+      action,
+      items: {
+        [MockModel.Department.id]: MockModel.Department
+      }
+    };
+    expect(reduce).deep.equal(expectedResult);
+  });
+
+  it('Remove', () => {
+    let action = ACTION_DEPARTMENT.removed(MockModel.Department);
+    let initialState = {
+      action,
+      items: {
+        [MockModel.Department.id]: MockModel.Department
+      }
+    };
+    let reduce = department(initialState, action);
+    let expectedResult: DepartmentStoreState = {
+      action,
+      items: {}
+    };
+    expect(reduce).deep.equal(expectedResult);
+  });
+});
