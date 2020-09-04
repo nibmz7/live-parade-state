9ca527400ba514c26d6bc8446974195f1ebde85b
nibmz7 <nibmz7@gmail.com>
2020-09-04 21:18:17 +0800

    Added user actions

--- data/actions/user_action.ts
+++ data/actions/user_action.ts
@@ -1,39 +1,29 @@
-import Department from '../../model/department';
-import {
-  DepartmentAction,
-  DepartmentActionError,
-  DepartmentPayload
-} from '../states/department_state';
 import { ACTION_ROOT, generateActionId } from '../store';
 import { ACTION_TYPE } from '../data_manager';
+import { UserAction, UserPayload, UserActionError } from '../states/user_state';
+import User from '../../model/user';
 
-const makeAction = (
-  type: ACTION_TYPE,
-  payload: DepartmentPayload
-): DepartmentAction => ({
-  root: ACTION_ROOT.DEPARTMENTS,
+const makeAction = (type: ACTION_TYPE, payload: UserPayload): UserAction => ({
+  root: ACTION_ROOT.USERS,
   id: generateActionId(),
   type,
   payload
 });
 
-const ACTION_DEPARTMENT = {
-  initialized: (departments: Array<Department>): DepartmentAction =>
-    makeAction(ACTION_TYPE.INITIALIZED, departments),
-  added: (department: Department): DepartmentAction =>
-    makeAction(ACTION_TYPE.ADDED, department),
-  modified: (department: Department): DepartmentAction =>
-    makeAction(ACTION_TYPE.MODIFIED, department),
-  removed: (department: Department): DepartmentAction =>
-    makeAction(ACTION_TYPE.REMOVED, department),
-  requestAdd: (department: Department): DepartmentAction =>
-    makeAction(ACTION_TYPE.REQUEST_ADD, department),
-  requestModify: (department: Department): DepartmentAction =>
-    makeAction(ACTION_TYPE.REQUEST_MODIFY, department),
-  requestRemove: (department: Department): DepartmentAction =>
-    makeAction(ACTION_TYPE.REQUEST_REMOVE, department),
-  error: (actionError: DepartmentActionError) =>
+const ACTION_USER = {
+  initialized: (Users: Array<User>): UserAction =>
+    makeAction(ACTION_TYPE.INITIALIZED, Users),
+  added: (User: User): UserAction => makeAction(ACTION_TYPE.ADDED, User),
+  modified: (User: User): UserAction => makeAction(ACTION_TYPE.MODIFIED, User),
+  removed: (User: User): UserAction => makeAction(ACTION_TYPE.REMOVED, User),
+  requestAdd: (User: User): UserAction =>
+    makeAction(ACTION_TYPE.REQUEST_ADD, User),
+  requestModify: (User: User): UserAction =>
+    makeAction(ACTION_TYPE.REQUEST_MODIFY, User),
+  requestRemove: (User: User): UserAction =>
+    makeAction(ACTION_TYPE.REQUEST_REMOVE, User),
+  error: (actionError: UserActionError) =>
     makeAction(ACTION_TYPE.REQUEST_ERROR, actionError)
 };
 
-export default ACTION_DEPARTMENT;
+export default ACTION_USER;
