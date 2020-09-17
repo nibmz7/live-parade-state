497fb0ebbfbd6925c4119d0550b4354836f0ebca
nibmz7 <nibmz7@gmail.com>
2020-09-17 22:56:57 +0800

    Added admin view template

--- ui/views/admin_view.ts
+++ ui/views/admin_view.ts
@@ -1,4 +1,6 @@
 import { LitElement, html, customElement, css } from 'lit-element';
+import MockAdminManager from '../../data-mock/mock_admin_manager';
+import { ACTION_ROOT, ApplicationStore } from '../../data/store';
 import { globalStyles } from '../global_styles';
 
 @customElement('admin-view')
@@ -9,6 +11,14 @@ export default class AdminView extends LitElement {
 
   connectedCallback() {
     super.connectedCallback();
+    ApplicationStore.listen(ACTION_ROOT.DEPARTMENTS, (state) => {
+      console.log(state);
+    });
+    ApplicationStore.listen(ACTION_ROOT.USERS, (state) => {
+      console.log(state);
+    });
+    let adminManager = new MockAdminManager();
+    adminManager.subscribe();
   }
 
   render() {
