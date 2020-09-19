14522c9995c21451b47a92459e4218211589f796
nibmz7 <nibmz7@gmail.com>
2020-09-19 13:40:03 +0800

    Update test config

--- karma.conf.js
+++ karma.conf.js
@@ -1,13 +1,10 @@
 const { createDefaultConfig } = require('@open-wc/testing-karma');
 const merge = require('deepmerge');
-const process = require('process');
-process.env.CHROME_BIN = require('puppeteer').executablePath()
 
 module.exports = config => {
   config.set(
     merge(createDefaultConfig(config), {
       files: [
-        {pattern: 'dist/**/*.js', type: 'module'},
         'test/global_variables.js',
         {
           pattern: config.grep ? config.grep : 'test/js/**/*.test.js',
