dc85fd3224fbb24e9152e81f9c100ca686af1c34
nibmz7 <nibmz7@gmail.com>
2020-10-18 18:37:33 +0800

    Update functions

--- functions/index.ts
+++ functions/index.ts
@@ -3,9 +3,16 @@ import * as functions from 'firebase-functions';
 
 admin.initializeApp();
 const db = admin.firestore();
-// const FieldValue = admin.firestore.FieldValue;
+const FieldValue = admin.firestore.FieldValue;
 const regionalFunction = functions.region('asia-northeast1');
 type FunctionContext = functions.https.CallableContext;
+
+const StatusDefault = () => ({
+  code: 0,
+  remarks: '',
+  updatedby: 'admin',
+  timestamp: FieldValue.serverTimestamp()
+});
 
 const checkIsAdmin = async (context: FunctionContext) => {
   if (!context.auth) {
@@ -45,6 +52,86 @@ export const deleteDepartment = regionalFunction.https.onCall(
         }
       }
       await db.doc(`branches/${adminid}/repository/${departmentid}`).delete();
+    }
+  }
+);
+
+export const createUser = regionalFunction.https.onCall(
+  async (data, context) => {
+    let isAdmin = await checkIsAdmin(context);
+    if (isAdmin) {
+      const { name, rank, regular, departmentid, emailPrefix, password } = data;
+      const branchid = context.auth!.uid;
+      const adminEmail = context.auth!.token.email!;
+      const domain = adminEmail.split('@')[1];
+      const email = `${emailPrefix}@${domain}`;
+      const repoRef = db.collection(`branches/${branchid}/repository`);
+      const user = await admin.auth().createUser({
+        email,
+        password
+      });
+      await admin
+        .auth()
+        .setCustomUserClaims(user.uid, { branchid, departmentid });
+      await repoRef.doc(user.uid).set({
+        type: 'user',
+        data: {
+          branchid,
+          departmentid,
+          name,
+          rank,
+          regular,
+          email,
+          status: {
+            am: StatusDefault(),
+            pm: StatusDefault()
+          }
+        }
+      });
+    }
+  }
+);
+
+export const updateUser = regionalFunction.https.onCall(
+  async (data, context) => {
+    const isAdmin = await checkIsAdmin(context);
+    if (isAdmin) {
+      let { uid, name, rank, regular, emailPrefix, departmentid } = data;
+      const branchid = context.auth!.uid;
+      const adminEmail = context.auth!.token.email!;
+      const domain = adminEmail.split('@')[1];
+      const email = `${emailPrefix}@${domain}`;
+      await admin.auth().updateUser(uid, { email });
+      await admin.auth().setCustomUserClaims(uid, { branchid, departmentid });
+      await db.doc(`branches/${branchid}/repository/${uid}`).update({
+        'data.name': name,
+        'data.rank': rank,
+        'data.regular': regular,
+        'data.email': email,
+        'data.departmentid': departmentid
+      });
+    }
+  }
+);
+
+export const updatePassword = regionalFunction.https.onCall(
+  async (data, context) => {
+    const isAdmin = await checkIsAdmin(context);
+    if (isAdmin) {
+      const uid = data.uid;
+      const password = data.password;
+      await admin.auth().updateUser(uid, { password });
+    }
+  }
+);
+
+export const deleteUser = regionalFunction.https.onCall(
+  async (data, context) => {
+    const isAdmin = await checkIsAdmin(context);
+    if (isAdmin) {
+      const adminid = context.auth!.uid;
+      await admin.auth().deleteUser(data.uid);
+      await db.doc(`branches/${adminid}/repository/${data.uid}`).delete();
     }
   }
 );
